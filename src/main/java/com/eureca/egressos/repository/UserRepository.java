ed26443c6b4c22dc28862d129f0e7376b85fe1a6
LS-Flerus <luizsergiopompeu@gmail.com>
2025-08-24 22:51:15 -0300

    correção no repositório de usuários

--- src/main/java/com/eureca/egressos/repository/UserRepository.java
+++ src/main/java/com/eureca/egressos/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.eureca.egressos.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +12,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
     Optional<UserModel> findByLogin(String login);
     Optional<UserModel> findByName(String name);
-    Optional<UserModel> getAllByCourseCode(String courseCode);
+    List<UserModel> getAllByCourseCode(String courseCode);
 }
