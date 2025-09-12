2b3b93cd8ecb9da15b0cfe3d508dab78dbcd68b4
LS-Flerus <luizsergiopompeu@gmail.com>
2025-09-12 14:08:29 -0300

    Adição das URLs do STI

--- src/main/java/com/eureca/egressos/util/Constants.java
+++ src/main/java/com/eureca/egressos/util/Constants.java
@@ -2,7 +2,7 @@ package com.eureca.egressos.util;
 
 public class Constants {
 
-    public static String domain = "lsd";
+    public static String domain = "sti";
     public static String dasUrl = "https://eureca."+domain+".ufcg.edu.br/das/v2/";
     public static String asUrl = "https://eureca."+domain+".ufcg.edu.br/as/";
     public static String nameAlumniStatus = "EGRESSOS";
